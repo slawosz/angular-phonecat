c2c92bdef234a9563a60dfe09ced07f6e4b6a875
Sławosz <slawosz@gmail.com>
2013-02-15 17:50:06 +0100

    my version of tutorial

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,21 @@
 'use strict';
 
-/* Controllers */
+function PhoneListCtrl($scope, $http) {
+  $scope.phones = Phone.query();
+
+  $scope.orderProp = 'age';
+}
+
+function PhoneDetailCtrl($scope, $routeParams, $http) {
+  $scope.phoneId = $routeParams.phoneId;
+
+  $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
+    $scope.phone = data;
+    $scope.mainImageUrl = $scope.phone.images[0];
+  });
+
+
+  $scope.setImage = function(imageUrl) {
+    $scope.mainImageUrl = imageUrl;
+  }
+}
